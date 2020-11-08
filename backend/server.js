939b0c6c935d1c6af72a29100a6a9cfcf69f31a9
Leonardo <leoigeltiko15@gmail.com>
2020-11-08 19:27:48 +0200

    app.use(express.static(path.join('frontend/build')))

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.get("/api/config/paypal", (req, res) =>
 app.use('/uploads', express.static(path.join(__dirname,'/uploads')))
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('frontend/build'));
+  app.use(express.static(path.join('frontend/build')));
 
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname + '/frontend/build/index.html'));

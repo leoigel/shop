30495cade406cd87017427c0665ea49495041404
Leonardo <leoigeltiko15@gmail.com>
2020-11-08 20:03:54 +0200

    prepare for deploy again

--- backend/server.js
+++ backend/server.js
@@ -34,5 +34,5 @@ app.get("*", (req, res) => {
   })
 }
 
-const PORT = process.env.PORT || 5027;
+const PORT = process.env.PORT || 5000;
 app.listen(5027, () => console.log(`server is running ${PORT}`.yellow.bold));

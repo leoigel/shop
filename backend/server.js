32bfadcb2fc8afb0685a3541b25b66613048eb11
Leonardo <leoigeltiko15@gmail.com>
2020-11-08 14:31:34 +0200

    build was added

--- backend/server.js
+++ backend/server.js
@@ -19,13 +19,15 @@ app.get("/api/config/paypal", (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
 
+
 app.use('/uploads', express.static(path.join(__dirname,'/uploads')))
 
 if (process.env.NODE_ENV === 'production') {
+  console.log(__dirname)
   app.use(express.static(path.join(__dirname, '/frontend/build')))
 
   app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname,'frontend','build','index.html'))
   )
 } else {
   app.get('/', (req, res) => {

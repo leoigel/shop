b764dd85e3b2ccbff4e3b35155b42858343d3c20
Leonardo <leoigeltiko15@gmail.com>
2020-11-08 20:16:21 +0200

    Port was changed

--- backend/server.js
+++ backend/server.js
@@ -34,5 +34,5 @@ app.get("*", (req, res) => {
   })
 }
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(5027, () => console.log(`server is running ${PORT}`.yellow.bold));

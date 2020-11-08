322f972636c8c3cd92d12c1a832878b1ad294f9b
Leonardo <leoigeltiko15@gmail.com>
2020-11-08 11:21:05 +0200

    prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -20,5 +20,18 @@ app.get("/api/config/paypal", (req, res) =>
 );
 
 app.use('/uploads', express.static(path.join(__dirname,'/uploads')))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
+}
+
 const PORT = process.env.PORT || 5027;
 app.listen(5027, () => console.log(`server is running ${PORT}`.yellow.bold));

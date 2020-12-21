7929cd7fa5173893e73c92143b33f52605a373f9
Leonardo <leoigeltiko15@gmail.com>
2020-12-21 13:01:47 +0200

    the path on line 28 was changed

--- backend/server.js
+++ backend/server.js
@@ -21,12 +21,11 @@ app.get("/api/config/paypal", (req, res) =>
 
 
 app.use('/uploads', express.static(path.join(__dirname,'/uploads')))
-console.log(path.join(__dirname + '/frontend/build'))
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname + '/frontend/build')));
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname + '/frontend/build/index.html'));
+    res.sendFile(path.join(__dirname + '/../frontend/build/index.html'));
 });
 } else {
   app.get('/', (req, res) => {
